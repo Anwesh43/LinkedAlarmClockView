6015ed51d09be8d6e372d39f786d446915729bc3
Anwesh43 <anweshthecool0@gmail.com>
2020-11-23 02:30:40 +0530

    adding constants and libraries

--- alarmclockview/src/main/java/com/example/alarmclockview/AlarmClockView.kt
+++ alarmclockview/src/main/java/com/example/alarmclockview/AlarmClockView.kt
@@ -1,2 +1,28 @@
 package com.example.alarmclockview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#3F51B5",
+    "#4CAF50",
+    "#03A9F4",
+    "#009688"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val sizeFactor : Float = 4.9f
+val rFactor : Float = 15.2f
+val backColor : Int = Color.parseColor("#BDBDBD")
+
