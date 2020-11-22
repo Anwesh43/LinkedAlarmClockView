eb1968c848f8ed134ffe87ee07c9ebd4c41857cc
Anwesh43 <anweshthecool0@gmail.com>
2020-11-23 02:58:49 +0530

    adding ACNode class

--- alarmclockview/src/main/java/com/example/alarmclockview/AlarmClockView.kt
+++ alarmclockview/src/main/java/com/example/alarmclockview/AlarmClockView.kt
@@ -124,4 +124,45 @@ class AlarmClockView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ACNode(var i : Int, val state : State = State()) {
+
+        private var next : ACNode? = null
+        private var prev : ACNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                this.next = ACNode(i + 1)
+                this.next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawACNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ACNode {
+            var curr : ACNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
