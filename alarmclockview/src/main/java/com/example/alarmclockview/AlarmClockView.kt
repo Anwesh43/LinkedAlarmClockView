27ee4108b675355cdb8c60f4ce251343fc57093a
Anwesh43 <anweshthecool0@gmail.com>
2020-11-23 02:41:57 +0530

    adding Canvas extension methods

--- alarmclockview/src/main/java/com/example/alarmclockview/AlarmClockView.kt
+++ alarmclockview/src/main/java/com/example/alarmclockview/AlarmClockView.kt
@@ -25,9 +25,39 @@ val delay : Long = 20
 val sizeFactor : Float = 4.9f
 val rFactor : Float = 15.2f
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 20f
+val gap : Float = 30f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
 
+fun Canvas.drawAlarmClock(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    save()
+    translate(w / 2, h / 2)
+    rotate(rot * (sf.divideScale(3, parts) - 2 * sf.divideScale(4, parts)))
+    drawCircle(0f, 0f, size * sf.divideScale(0, parts), paint)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        rotate(gap * sf.divideScale(2, parts))
+        translate(0f, -size - r)
+        drawArc(RectF(-r, -r, r, r), 0f, 180f * sf.divideScale(1, parts), true, paint)
+        restore()
+    }
+    restore()
+}
+
+
+fun Canvas.drawACNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawAlarmClock(scale, w, h, paint)
+}
